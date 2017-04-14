660b7ab28b85513c3eb95c2d4c301113da0999cc
Zachary Slayter <zslayter@credera.com>
2017-04-14 12:12:27 -0500

    Added icons, images, and started organizing code
    The app icon has been added, the launch screen has been added, and the
    code has begun to be organized in separate folders. The storyboard is
    also being broken apart in multiple files.

---
+++ Shapes/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Shapes
+//
+//  Created by Zachary Slayter on 4/14/17.
+//  Copyright © 2017 Credera. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    enum TabBarScreens : Int {
+        case Home = 0
+        case MyRewards = 1
+        case OrderNow = 2
+        case More = 3
+    }
+
+}
---
+++ Shapes/MainTabBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabBarViewController.swift
+//  Shapes
+//
+//  Created by Zachary Slayter on 4/14/17.
+//  Copyright © 2017 Credera. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var viewControllerArray = Array<UIViewController>()
+        
+        // Initialize the Tab Bar controllers via their respective storyboards
+        // The TabBarItems with titles & icons are in each Storyboard's first scene
+        
+        let homeVC : UIViewController? = UIStoryboard(name: "Home", bundle: nil).instantiateInitialViewController()
+        if homeVC != nil {
+            viewControllerArray.append(homeVC!)
+        }
+        
+        let exploreVC : UIViewController? = UIStoryboard(name: "Explore", bundle: nil).instantiateInitialViewController()
+        if exploreVC != nil {
+            viewControllerArray.append(exploreVC!)
+        }
+        
+        self.setViewControllers(viewControllerArray, animated: false)
+        
+        // Default to the Home Tab
+        self.setSelectedTab(Constants.TabBarScreens.Home)
+        
+    }
+    
+    func setSelectedTab(_ tabBarScreen : Constants.TabBarScreens) {
+        self.selectedIndex = tabBarScreen.rawValue
+    }
+    
+}
